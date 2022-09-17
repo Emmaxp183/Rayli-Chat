2a2f03b19e15aa94de9e9f9f3a94c09e8f1cd5ba
Emmanuel Mensah <immaxp183@gmail.com>
2022-09-17 22:49:25 +0200

    Setting up my Project

---
+++ Rayli Chat/Rayli Chat/Rayli_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rayli_ChatApp.swift
+//  Rayli Chat
+//
+//  Created by macbook on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rayli_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
